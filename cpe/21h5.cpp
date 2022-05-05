e3d17f7fc1f1359a0544d3a4b4f45c95b91ae0da
Kaka <ka.kusaowo@gmail.com>
2022-05-05 17:07:24 +0800

    Update 21h5.cpp

--- cpe/21h5.cpp
+++ cpe/21h5.cpp
@@ -7,38 +7,13 @@ int offsetByChar(char c) {
     return 0;
 }
 
-void doCase(int n) {
-    char data[51];
-    scanf("%s", data);
-    
-    // 1. Calculate the height of the graph
+void calcOrPlot(char *data, int *pH, int *pOffset, int *pLen, char *map) {
     int y = 0, h = 0, offset = 0, len = 0;
+    if (pH) h = *pH;
+    if (pOffset) offset = *pOffset;
+    if (pLen) len = *pLen;
+    
     char prev = 'C'; // "R"ise, "F"all, "C"onstant
-    for (int i=0; data[i]; i++, len++) {
-        char c = data[i];
-        if (prev == c) {
-            y += offsetByChar(c);
-        } else {
-            if (c == 'C' && prev == 'R') y++;
-            if (c == 'F' && prev == 'C') y--;
-            prev = c;
-        }
-        
-        if (offset > y) offset = y;
-        if (h < y) h = y;
-    }
-    
-    if (offset > 0) offset = 0;
-    if (offset < 0) h += -offset;
-    h++;
-    
-    // 2. Build a map and store data
-    //    Index is based on (len * y) + x
-    char *map = malloc(h * len);
-    memset(map, ' ', h * len);
-    
-    y = 0;
-    prev = 'C';
     for (int i=0; data[i]; i++) {
         char c = data[i];
         if (prev == c) {
@@ -49,27 +24,58 @@ void doCase(int n) {
             prev = c;
         }
         
-        char symb = '_';
-        switch(c) {
-            case 'R':
-                symb = '/';
-                break;
-            case 'F':
-                symb = '\\';
-                break;
+        if (map) {
+            char symb = '_';
+            switch(c) {
+                case 'R':
+                    symb = '/';
+                    break;
+                case 'F':
+                    symb = '\\';
+                    break;
+            }
+            map[len * (y-offset) + i] = symb;
+        } else {
+            if (offset > y) offset = y;
+            if (h < y) h = y;
+            len++;
         }
-        map[len * (y-offset) + i] = symb;
     }
+    
+    if (!map) {
+        if (offset > 0) offset = 0;
+        if (offset < 0) h += -offset;
+        h++;
+    
+        if (pH) *pH = h;
+        if (pOffset) *pOffset = offset;
+        if (pLen) *pLen = len;
+    }
+}
+
+void doCase(int n) {
+    char data[51];
+    scanf("%s", data);
+    
+    // 1. Calculate the height of the graph
+    int h = 0, offset = 0, len = 0;
+    calcOrPlot(data, &h, &offset, &len, 0);
+    
+    // 2. Build a map and store data
+    char *map = malloc(h * len);
+    memset(map, ' ', h * len);
+    calcOrPlot(data, &h, &offset, &len, map);
     
     printf("Case #%d:\n", n);
     
-    // 3. Print the graph
-     for (int i=0; i<h; i++) {
+    // 3. Write 0s to trim every line
+    for (int i=0; i<h; i++) {
         for (int j=len-1; map[len * (h-i-1) + j] == ' '; j--) {
             map[len * (h-i-1) + j] = 0;
         }
     }
     
+    // 4. Print the graph
     for (int i=0; i<h; i++) {
         printf("| ");
         for (int j=0; map[len * (h-i-1) + j] != 0 && j < len; j++) {
